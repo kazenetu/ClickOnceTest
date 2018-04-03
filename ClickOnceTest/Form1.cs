1e080043ab255d451b1c7be62a8609dbf182e301
kazenetu <rakudafactory@gmail.com>
2018-04-03 23:25:35 +0900

    サンプルプログラムの追加

---
+++ ClickOnceTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickOnceTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ApplicationDeployment.IsNetworkDeployed)
+            {
+                return;
+            }
+
+            // 起動URLを表示
+            MessageBox.Show(ApplicationDeployment.CurrentDeployment.ActivationUri.AbsoluteUri);
+        }
+    }
+}
