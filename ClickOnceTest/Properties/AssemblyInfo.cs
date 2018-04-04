cfb40761492c362c8370beb9bc4c17c4a98598fa
kazenetu <rakudafactory@gmail.com>
2018-04-04 22:13:17 +0900

    発行したClickOnceが正しくインストールできるように修正

--- ClickOnceTest/Properties/AssemblyInfo.cs
+++ ClickOnceTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClickOnceTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("kazenetu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
